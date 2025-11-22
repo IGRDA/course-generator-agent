470ce49464c00960e85f2e6e026c6bbbb3710748
Iñaki <inaki@Inakis-MacBook-Air.local>
2025-11-22 10:20:14 +0100

    change bing name

--- tools/imagesearch/__init__.py
+++ tools/imagesearch/__init__.py
@@ -1,6 +1,6 @@
 from .openverse import search_openverse_images
 from .ddg import search_ddg_images
-from .bing_scraper import search_bing_images
+from .bing import search_bing_images
 
 __all__ = [
     "search_openverse_images",
